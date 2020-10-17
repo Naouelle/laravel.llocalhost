d96ee4cd108acd0f49e43bb674f5b2b751051669
naouelle <emmabuscher@gmail.com>
2020-10-17 18:57:31 +0200

    Les controles structures

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,22 +7,12 @@ namespace App\Http\Controllers;
 class ArticleController extends Controller
 {
     /**
-     * @returns \Illuminate\Contracts\View\Factory\Illuminate\View\View
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function index()
     {
-        //$url = route('article', ['id' => 15]);
-        //dd($url);
-        //return redirect()->route('article', ['id' => 16]);
-
         $data = ['a', 'b'];
 
-        // dd($data);
-
-        \Debugbar::info($data);
-        \Debugbar::addMessage('Debug', 'mon label');
-
-        return view( 'article.index', ['articles' => $data]);
+        return view('article.index', ['data' => $data]);
     }
-
 }
--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -9,4 +9,19 @@
 
 @section('content')
     <p>Article content</p>
+
+    @if (count($data) === 1)
+        Mon tableau possède un élément
+    @elseif( count($data) > 1)
+        Mon tableau est > 1
+    @else
+        Mon tableau est vide
+    @endif
+
+    <ul>
+    @foreach($data as $d)
+        <li>{{ $d }}</li>
+    @endforeach
+    </ul>
+
 @endsection
